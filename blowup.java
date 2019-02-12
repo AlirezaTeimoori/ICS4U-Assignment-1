920829e10c279513e2512a6994c79c6ad2a7a67a
alireza <alirezateimoori80@gmail.com>
2019-02-11 23:08:10 -0500

    feat: java blowup file is created and some code is written

---
+++ blowup.java
@@ -0,0 +1,24 @@
+//-------------------------------------------
+//-- Created by:     Alireza Teimoori      --
+//-- Created on:     Feb 11 2019           --
+//-- Created for:    Assignment 1 - blowup --
+//-- Course Code:    ICS4U                 --
+//-- Teacher Name:   Chris Atkinson        --
+//-------------------------------------------
+
+//import java.util.*;
+import java.lang.StringBuffer;
+
+public class blowup{
+    public static StringBuffer blowUp(String input){
+        StringBuffer output = new StringBuffer();
+        System.out.println("func is started!");
+        return output;
+
+    }
+    public static void main(String[] args) {
+        blowup.blowUp("a3fb4");
+    }
+        
+    
+}
