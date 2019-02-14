22376dd9f5d1e85ce76a5c1f5a8c70d0a8057025
alireza <alirezateimoori80@gmail.com>
2019-02-13 19:31:32 -0500

    fix: wrote the program and it actually worksss!!!!

--- blowup.java
+++ blowup.java
@@ -7,17 +7,48 @@
 //-------------------------------------------
 
 //import java.util.*;
-import java.lang.StringBuffer;
 
 public class blowup{
-    public static StringBuffer blowUp(String input){
-        StringBuffer output = new StringBuffer();
+    public static String blowUp(String input){
+        String output = new String();
         System.out.println("func is started!");
+
+        for (int counter = 0; counter < input.length(); counter ++){ // Goes through all the characters in input
+            if (counter < 1){System.out.println("for has started!");} // Notifies that this loop has started
+
+            char charNow = input.charAt(counter); // finds what character the index is and puts it into "charNow"
+            char charNext;
+            int charValue = Character.getNumericValue(charNow);
+            System.out.println(charNow);
+            if (Character.isDigit(charNow)){ // Checks if the character is a number
+                System.out.println(" --- is digit:" + charNow);
+                try {
+                    charNext = input.charAt(counter+1);
+                    if (Character.isAlphabetic(charNext)){
+                        System.out.println(" --- but this is not a digit: " + charNext);
+                        for (int count = 0; count < charValue; count++){
+                            if (count < 1) {System.out.println("* " + charValue + " reproduction begins: \n");}
+                            
+                            output = output + charNext;
+                            
+                        }
+                    } else if (Character.isDigit(charNow)){
+                        output = output + charNow;
+                    }
+                } catch (Exception e) {
+                    System.out.println("catch! muhahahaha!!!!");
+                    output = output + charNow;
+                }
+            } else if (Character.isAlphabetic(charNow)){
+                output = output + charNow;
+            }
+            
+        }
         return output;
 
     }
     public static void main(String[] args) {
-        blowup.blowUp("a3fb4");
+        System.out.println(blowup.blowUp("a3fb4d59"));
     }
         
     
