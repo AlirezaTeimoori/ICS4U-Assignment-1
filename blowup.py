b0cd5bb7e742167d4872e3b2827bd37c13b1a7e7
alireza <alirezateimoori80@gmail.com>
2019-02-13 21:39:59 -0500

    feat: create blowup.py file

---
+++ blowup.py
@@ -0,0 +1,15 @@
+'''
+----------------------------------------------------------------
+-- Created by:     Alireza Teimoori      -----------------------
+-- Created on:     Feb 9 2019            -----------------------
+-- Created for:    Assignment 1 - blowup -----------------------
+-- Course Code:    ICS4U                 -----------------------
+-- Teacher Name:   Chris Atkinson        -----------------------
+----------------------------------------------------------------
+This program a creates a function called "blowUp()"   ----------
+This function makes sure that each digit 0-9 that     ----------
+appears in the original string is replaced by that    ----------
+many occurrences of the character to the right of the digit. ---
+----------------------------------------------------------------
+'''
+
